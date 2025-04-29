5dce550dee5f6f973699caefbc6a3baa45953929
WSommerfeld <marcinlasak5@gmail.com>
2025-04-29 02:45:22 +0200

    /new

--- lab3/lab3/app/components/Navbar.jsx
+++ lab3/lab3/app/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../style.css'; 
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><a href="#">Strona Główna</a></li>
-        <li><a href="#">Dodaj książkę</a></li>
+		<li><Link to="/">Strona Główna</Link></li>
+        <li><Link to="/new">Dodaj książkę</Link></li>
         <li><a href="#">Koszyk</a></li>
         <li><a href="#">Logowanie</a></li>
       </ul>
